56b47ce353d0ccf339a33dabf7bf6656bfc7ee21
pvsaiganesh <ipvsaiganesh@gmail.com>
2025-01-04 11:25:32 +0530

    up

--- index.js
+++ index.js
@@ -45,3 +45,14 @@ headerLogoConatiner.addEventListener("click", () => {
 // gmailEl2?.onclick  = () => {
 //   navigator.clipboard.writeText("+917780531969");
 // };
+function toggleDarkMode() {
+  let isDark = document.body.classList.toggle("dark-mode");
+  localStorage.setItem("darkMode", isDark ? "enabled" : "disabled");
+}
+
+// On page load
+document.addEventListener("DOMContentLoaded", (event) => {
+  if (localStorage.getItem("darkMode") === "enabled") {
+    document.body.classList.add("dark-mode");
+  }
+});
